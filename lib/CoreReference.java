bae16a92a0838f7c73153e1f092af31d5ed39502
TheCountryGamer <dustinyost@Dustins-MacBook-Pro.local>
2014-01-14 17:43:30 -0500

    Prep for release of 1.1.1

--- lib/CoreReference.java
+++ lib/CoreReference.java
@@ -7,7 +7,7 @@ public class CoreReference {
 	public static final String MOD_ID_LOWERCASE = CoreReference.MOD_ID.toLowerCase();
 	public static final String BASE_TEX = CoreReference.MOD_ID_LOWERCASE + ":";
 	public static final String MOD_NAME = "CountryGamer Core";
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.2.0";
 	public static final String MC_VERSION = "1.6.4";
 	public static final String CHANNEL_NAME = MOD_ID;
 	public static final String SERVER_PROXY_CLASS = "CountryGamer_Core.Proxy.ServerProxy";
