c895abea98ef78f4dd496dbefddfbe3e7c6f8859
TheCountryGamer <dustinyost@Dustins-MacBook-Pro.local>
2014-04-27 11:34:32 -0400

    Fixed version #

--- java/com/countrygamer/core/common/lib/CoreReference.java
+++ java/com/countrygamer/core/common/lib/CoreReference.java
@@ -5,7 +5,7 @@ public class CoreReference {
 	public static final String MOD_ID = "core";
 	public static final String BASE_TEX = CoreReference.MOD_ID + ":";
 	public static final String MOD_NAME = "CountryGamer Core";
-	public static final String VERSION = "2.0.0";
+	public static final String VERSION = "2.0.2";
 	public static final String MC_VERSION = "1.7.2";
 	public static final String CHANNEL_NAME = MOD_ID;
 	public static final String SERVER_PROXY_CLASS = "com.countrygamer."
