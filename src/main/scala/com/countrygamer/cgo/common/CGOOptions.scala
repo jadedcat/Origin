83670fd6aae9d682faa1cf0fe1156abe268cdc3d
TheCountryGamer <the.country.gamer@gmail.com>
2014-08-12 14:38:32 -0400

    Removed demo code

--- src/main/scala/com/countrygamer/cgo/common/CGOOptions.scala
+++ src/main/scala/com/countrygamer/cgo/common/CGOOptions.scala
@@ -28,19 +28,6 @@ object CGOOptions extends OptionRegister {
 			true
 		)
 
-		val a1: Int = Config.getAndComment(this.config, "A Cat", "a1", "comment", 1)
-		val a2: Int = Config.getAndComment(this.config, "A Cat", "a2", "comment", 2)
-		val b1: Int = Config.getAndComment(this.config, "B Cat", "b1", "comment", 3)
-		val b2: Int = Config.getAndComment(this.config, "B Cat", "b2", "comment", 4)
-		val c1: Array[Boolean] = Config.getAndComment(this.config, "C Cate", "c1", "comment",
-			Array[Boolean](true, false, true))
-		val c2: Array[Int] = Config
-				.getAndComment(this.config, "C Cate", "c2", "comment", Array[Int](1, 2, 3))
-		val c3: Array[Double] = Config
-				.getAndComment(this.config, "C Cate", "c3", "comment", Array[Double](1.1, 2.2, 3.3))
-		val c4: Array[String] = Config
-				.getAndComment(this.config, "C Cate", "c4", "comment", Array[String]("x", "y", "z"))
-
 	}
 
 	@SideOnly(Side.CLIENT)
